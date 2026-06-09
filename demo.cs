8ef08d4a0fb8624d35756684fa25a9d046b1070d
RaghavMaruthuRaj <raghavmaruthuraj2005@gmail.com>
2026-06-09 17:44:28 +0530

    Add demo.cs to calculate the sum of two numbers

---
+++ demo.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        Console.WriteLine("Sum = " + sum);
+    }
+}
