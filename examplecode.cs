d1b9976f2e69c9c864f30f269f6cdb60949c1eea
ayman-developer <trendyayman@gmail.com>
2026-06-14 15:24:42 +0530

    Add Program class with multiple welcome messages

---
+++ examplecode.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome");
+        Console.WriteLine("Welcome");
+        Console.WriteLine("Welcome");
+        Console.WriteLine("Welcome");
+    }
+}
