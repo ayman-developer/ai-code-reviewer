87a716ed4935ce77ac452d628ab94b273bd83ecb
RaghavMaruthuRaj <raghavmaruthuraj2005@gmail.com>
2026-06-14 13:48:26 +0530

    Create demoraghav.cs

---
+++ demoraghav.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static int Divide(int a, int b)
+    {
+        return a / b
+    }
+
+    static void Main()
+    {
+        int x = "100";
+
+        Console.WriteLine(y);
+
+        Console.WriteLine(Divide(10, 0));
+    }
+}
