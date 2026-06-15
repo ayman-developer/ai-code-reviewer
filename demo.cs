2d3de94ae50c4fba598e23fe7d5be247e60baa4d
vishva1930 <guruvishvav97463@gmail.com>
2026-06-15 17:29:30 +0530

    Create demo.cs

---
+++ demo.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = 0;
+
+        Console.WriteLine(a / b);
+
+        string name = null;
+        Console.WriteLine(name.Length);
+
+        int x = 5;
+        int y = 10;
+        int z = x + y;
+
+        Console.WriteLine(z);
+    }
+}
